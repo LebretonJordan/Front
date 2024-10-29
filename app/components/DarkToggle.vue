bf090e4efe387c311216e02864ea73cbe94b674c
JB AUBREE <jeanbaptiste.aubree@consulting-for.accor.com>
2024-10-29 16:18:22 +0100

    chore: update

---
+++ app/components/DarkToggle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const changeColor = () => (colorMode.preference = (colorMode.value === 'light' ? 'dark' : 'light'))
+</script>
+
+<template>
+  <button class="!outline-none" @click="changeColor">
+    <div class="i-carbon-sun dark:i-carbon-moon" />
+  </button>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const online = useOnline()
+</script>
+
+<template>
+  <div>
+    <Suspense>
+      <ClientOnly>
+        <PageView v-if="online" />
+        <div v-else class="text-gray:80">
+          You're offline
+        </div>
+      </ClientOnly>
+      <template #fallback>
+        <div class="italic op50">
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+    My page
+    <DarkToggle />
+  </div>
+</template>
