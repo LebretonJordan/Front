581f8703977f7da54dc78d425077db6e42ef4af6
LebretonJordan <jordanlebreton94@gmail.com>
2024-11-04 18:35:29 +0100

    Inscription et connnexion

---
+++ app/pages/connexion.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const form = ref({
+  email: '',
+  password: '',
+})
+
+async function handleFormSubmit() {
+  const response = await $fetch('http://127.0.0.1:8000/api/connexion', {
+    method: 'POST',
+    body: form.value,
+  })
+
+  if (response) {
+    console.log(response)
+  }
+
+  console.log('Connexion réussie')
+}
+</script>
+
+<template>
+  <div>
+    <h1>Connexion</h1>
+
+    <form>
+      <div>
+        <label for="email">Email</label>
+        <input id="email" v-model="form.email" type="email" placeholder="Email" required>
+      </div>
+      <div>
+        <label for="password">Mot de passe</label>
+        <input id="password" v-model="form.password" type="password" placeholder="Mot de passe" required>
+      </div>
+
+      <div>
+        <button @click.prevent="handleFormSubmit">
+          Se connecter
+        </button>
+      </div>
+      <p>Si vous n'avez pas de compte, veuillez vous inscrire ici</p>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.alert {
+  padding: 1em;
+  margin-bottom: 1em;
+}
+.alert-danger {
+  color: #a94442;
+  background-color: #f2dede;
+}
+</style>
--- app/pages/inscription.vue
+++ app/pages/inscription.vue
@@ -1,131 +1,88 @@
+<script setup lang="ts">
+const form = ref({
+  name: '',
+  lastname: '',
+  society: '',
+  email: '',
+  phone_number: '',
+  password: '',
+  adress: '',
+})
+// const { data, error, execute } = useFetch('http://127.0.0.1:8000/api/inscription', {
+//   immediate: false,
+//   watch: false,
+//   method: 'POST',
+//   body: form.value,
+
+//   mode: 'cors',
+// })
+async function handleFormSubmit() {
+  await $fetch('http://127.0.0.1:8000/api/inscription', {
+    method: 'POST',
+    body: form.value,
+  })
+}
+</script>
+
 <template>
   <div>
     <h1>Créer votre compte</h1>
     <p>Vous avez déjà un compte ? Connectez-vous !</p>
+    <!-- <div v-if="data" class="alert alert-success">
+      {{ data }}
+    </div> -->
 
     <!-- Affichage des messages de succès ou d'erreur -->
-    <div v-if="successMessage" class="alert alert-success">{{ successMessage }}</div>
-    <div v-if="errors.length" class="alert alert-danger">
-      <ul>
-        <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
-      </ul>
-    </div>
+    <!-- <div v-if="error" class="alert alert-danger">
+      <p>
+        {{ error }}
+      </p>
+    </div> -->
 
-    <form @submit.prevent="handleSubmit">
+    <form>
       <div>
         <label for="name">Prénom</label>
-        <input v-model="form.name" type="text" id="name" placeholder="Prénom" />
-        <span v-if="fieldErrors.name" class="error">{{ fieldErrors.name }}</span>
+        <input id="name" v-model="form.name" type="text" placeholder="Prénom">
       </div>
 
       <div>
         <label for="lastname">Nom</label>
-        <input v-model="form.lastname" type="text" id="lastname" placeholder="Nom" />
-        <span v-if="fieldErrors.lastname" class="error">{{ fieldErrors.lastname }}</span>
+        <input id="lastname" v-model="form.lastname" type="text" placeholder="Nom">
       </div>
 
       <div>
         <label for="society">Société (facultatif)</label>
-        <input v-model="form.society" type="text" id="society" placeholder="Société" />
+        <input id="society" v-model="form.society" type="text" placeholder="Société">
       </div>
 
       <div>
         <label for="email">Email</label>
-        <input v-model="form.email" type="email" id="email" placeholder="Email" />
-        <span v-if="fieldErrors.email" class="error">{{ fieldErrors.email }}</span>
+        <input id="email" v-model="form.email" type="email" placeholder="Email">
       </div>
 
       <div>
         <label for="phone_number">Téléphone</label>
-        <input v-model="form.phone_number" type="tel" id="phone_number" placeholder="Téléphone" />
-        <span v-if="fieldErrors.phone_number" class="error">{{ fieldErrors.phone_number }}</span>
+        <input id="phone_number" v-model="form.phone_number" type="tel" placeholder="Téléphone">
       </div>
 
       <div>
         <label for="password">Mot de passe</label>
-        <input v-model="form.password" type="password" id="password" placeholder="Mot de passe" />
-        <span v-if="fieldErrors.password" class="error">{{ fieldErrors.password }}</span>
+        <input id="password" v-model="form.password" type="password" placeholder="Mot de passe">
       </div>
 
       <div>
         <label for="adress">Adresse Principale</label>
-        <input v-model="form.adress" type="text" id="adress" placeholder="Adresse Principale" />
-        <span v-if="fieldErrors.adress" class="error">{{ fieldErrors.adress }}</span>
+        <input id="adress" v-model="form.adress" type="text" placeholder="Adresse Principale">
       </div>
 
       <div>
-        <button type="reset" @click="resetForm">Réinitialiser</button>
-        <button type="submit">Enregistrer</button>
+        <button @click.prevent="handleFormSubmit">
+          Enregistrer
+        </button>
       </div>
     </form>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      form: {
-        name: '',
-        lastname: '',
-        society: '',
-        email: '',
-        phone_number: '',
-        password: '',
-        adress: ''
-      },
-      successMessage: '',
-      errors: [],
-      fieldErrors: {}
-    };
-  },
-  methods: {
-    async handleSubmit() {
-      this.errors = [];
-      this.fieldErrors = {};
-
-      try {
-        // Envoie le formulaire à l'API
-        const response = await fetch('/api/register', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(this.form)
-        });
-        const data = await response.json();
-
-        if (!response.ok) {
-          if (data.errors) {
-            this.fieldErrors = data.errors; // erreurs spécifiques à chaque champ
-          } else {
-            this.errors.push(data.message || 'Une erreur est survenue');
-          }
-        } else {
-          this.successMessage = 'Compte créé avec succès !';
-          this.resetForm();
-        }
-      } catch (error) {
-        this.errors.push('Erreur de connexion au serveur.');
-      }
-    },
-    resetForm() {
-      this.form = {
-        name: '',
-        lastname: '',
-        society: '',
-        email: '',
-        phone_number: '',
-        password: '',
-        adress: ''
-      };
-      this.successMessage = '';
-      this.errors = [];
-      this.fieldErrors = {};
-    }
-  }
-};
-</script>
 
 <style scoped>
 .error {
